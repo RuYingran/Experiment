af8cd4b8257a7badf84413c5e0afccd6ad382034
RuYingran <109326479+RuYingran@users.noreply.github.com>
2022-07-30 11:19:46 +0800

    sm3_thread

---
+++ sm3_thread/sm3_thread.cpp
@@ -0,0 +1,29 @@
+﻿#include<iostream>
+#include <string.h>
+#include <stdio.h>
+#include <string>
+#include"sm3_thread.h"
+using namespace std;
+
+int main()
+{
+	printf("please enter the message:\n");
+	std::string input_str;
+	std::cin >> input_str;
+	int dlen = input_str.size();
+
+	unsigned char* message = (unsigned char*)input_str.c_str();
+	unsigned char res[32];
+
+	printf("Message:\n%s\n", message);
+
+	sm3(message, dlen, res);
+	printf("Hash:\n");
+	for (int i = 0; i < 32; i++)
+	{
+		printf("%02x", res[i]);
+		if (((i + 1) % 4) == 0) printf(" ");
+	}
+	printf("\n");
+}
+
